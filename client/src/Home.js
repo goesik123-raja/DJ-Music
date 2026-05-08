fe5bea9828e5030c6f459d89c4d95d7cd3e3dd91
goesik123-raja <goesik123@gmail.com>
2026-05-08 13:27:31 +0530

    commit

--- client/src/Home.js
+++ client/src/Home.js
@@ -45,7 +45,7 @@ const Home = () => {
 
                 <div className="home-content-1">
                     <h1>Meet agaso's vibe</h1>
-                    <h3>Bold, electric, and alive—agaso brings the pulse of nightlife straight to your screen with
+                    <h3>Bold, electric, and alive—agaso brings the pulse of nightlife straight to your screen with<br></br>
                         neon beats and sharp visuals.</h3>
                     <p>150+
                         <br></br>
